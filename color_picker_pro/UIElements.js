6d179084e4a55d814fd69754a819593590d68909
Alexander Deplov <deplov@icloud.com>
2022-12-12 19:24:00 +0100

    New

---
+++ color_picker_pro/UIElements.js
@@ -0,0 +1,43 @@
+function createBGColorSwitches(){
+var path = new Path.Rectangle()
+
+  // var path = new Path.Rectangle({
+  //   size: [140, 30],
+  //   fillColor: bgColorsArray[activeBGcolorIndex],
+  //   position: [view.center.x, 50],
+  //   parent: bgColorGroup
+  // })
+  // path.fillColor.saturation = 0.2
+  //
+  // path.onMouseEnter = function(event){
+  //   path.fillColor.lightness = 1
+  // }
+  // path.onMouseLeave = function(event){
+  //   path.fillColor.lightness = '0.911764705882353'
+  // }
+  //
+  // var label = new PointText({
+  //     point: path.position + [0, 4],
+  //     content: 'Background color',
+  //     fillColor: '#333',
+  //     fontSize: 14,
+  //     justification: 'center',
+  //     locked: true,
+  //     parent: bgColorGroup
+  // })
+  //
+  // roundCorners(path, 17)
+  //
+  // bgColorGroup.onMouseUp = function(event){
+  //   bg.fillColor = bgColorsArray[activeBGcolorIndex]
+  //   bordersAround.strokeColor = borderColorArray[activeBGcolorIndex]
+  //   compoundPathOfPlusIcon.strokeColor = borderColorArray[activeBGcolorIndex]
+  //   createNewButton.fillColor = bg.fillColor
+  //   activeBGcolorIndex += 1
+  //   if (activeBGcolorIndex == bgColorsArray.length){
+  //     activeBGcolorIndex = 0
+  //   }
+  //
+  // }
+
+}
